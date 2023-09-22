3408ae7b8781e32a2bb1e4cd29582e14ee7cb7b1
Benaliyoussef1 <yb1979383@gmail.com>
2023-09-22 22:13:03 +0100

    lol

--- _atoi.c
+++ _atoi.c
@@ -1,37 +1,40 @@
 #include "shell.h"
 
 /**
- * interactive - returns true if shell is interactive mode
- * @info: struct address
+ * check_interactive - Check if the shell is in interactive mode
+ * @info: Pointer to the info_t struct
  *
- * Return: 1 if interactive mode, 0 otherwise
+ * Return: 1 if in interactive mode, 0 otherwise
  */
-int interactive(info_t *info)
+int check_interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
- * is_delim - checks if character is a delimeter
- * @c: the char to check
- * @delim: the delimeter string
+ * is_delimiter - Check if a character is a delimiter
+ * @c: The character to check
+ * @delimiters: The delimiter string
+ *
  * Return: 1 if true, 0 if false
  */
-int is_delim(char c, char *delim)
+int is_delimiter(char c, char *delimiters)
 {
-	while (*delim)
-		if (*delim++ == c)
+	while (*delimiters)
+	{
+		if (*delimiters++ == c)
 			return (1);
+	}
 	return (0);
 }
 
 /**
- * _isalpha - checks for alphabetic character
- * @c: The character to input
+ * is_alpha - Check if a character is alphabetic
+ * @c: The character to check
+ *
  * Return: 1 if c is alphabetic, 0 otherwise
  */
-
-int _isalpha(int c)
+int is_alpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
@@ -40,12 +43,12 @@ int _isalpha(int c)
 }
 
 /**
- * _atoi - converts a string to an integer
- * @s: the string to be converted
- * Return: 0 if no numbers in string, converted number otherwise
+ * custom_atoi - Convert a string to an integer
+ * @s: The string to be converted
+ *
+ * Return: 0 if no numbers in string, the converted number otherwise
  */
-
-int _atoi(char *s)
+int custom_atoi(char *s)
 {
 	int i, sign = 1, flag = 0, output;
 	unsigned int result = 0;
